15864a1bd485da127130f1a0d4386c94b8e55ba8
Jérôme Rancati <jejemontagne@users.noreply.github.com>
2025-05-06 14:16:33 +0200

    Add files via upload

---
+++ content.js
@@ -0,0 +1,4 @@
+// This file is needed to enable communication between the background script and the page
+// It doesn't need to contain any code for the current implementation, as the background script
+// handles all the functionality via the chrome.scripting API
+console.log("GitHub PR Whitespace Hider content script loaded");
